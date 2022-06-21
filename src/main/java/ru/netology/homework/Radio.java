c753fdd69af407b320005cc01089c06fb6cdead2
FairyGreta <Christina_Art@mail.ru>
2022-06-21 11:45:08 +0500

    Тесты и класс дописаны

--- src/main/java/ru/netology/homework/Radio.java
+++ src/main/java/ru/netology/homework/Radio.java
@@ -2,19 +2,20 @@ package ru.netology.homework;
 
 public class Radio {
 
-    // Блок 1. Переключение радиостаций
+    // Поля
     public int currentRadioStation;
+    public int currentVolume;
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
     }
 
-    // сеттер
+    // Блок 1. Переключение радиостаций. Сеттер
     public void setCurrentRadioStation(int newCurrentRadioStation) {
-        if (newCurrentRadioStation < 0) {
+        if (newCurrentRadioStation <= 0) {
             return;
         }
-        if (newCurrentRadioStation > 9) {
+        if (newCurrentRadioStation >= 9) {
             return;
         }
 
@@ -32,14 +33,46 @@ public class Radio {
     }
 
     // метод prev
-//        public void prevRadioStation() {
-//            if (currentRadioStation > 0) {
-//                currentRadioStation = currentRadioStation - 1;
-//            } else if (currentRadioStation == 0) {
-//                currentRadioStation = 9;
-//            }
-//        }
+    public void prevRadioStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation = currentRadioStation - 1;
+        } else if (currentRadioStation == 0) {
+            currentRadioStation = 9;
+        }
+    }
 
     // Блок 2. Громкость
+    public int getCurrentVolume () {
+        return currentVolume;
+    }
 
+    // Сеттер
+    public void setCurrentVolume (int newCurrentVolume) {
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 10) {
+            return;
+        }
+
+        currentVolume = newCurrentVolume;
+    }
+
+    //Увеличение громкости
+    public void upCurrentVolume () {
+        if (currentVolume >= 0 && currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        } else if (currentVolume == 10) {
+            currentVolume = 10;
+        }
+    }
+
+    //Уменьшение громкости
+    public  void downCurrentVolume () {
+        if (currentVolume > 0 && currentVolume <= 10) {
+            currentVolume = currentVolume - 1;
+        } else if (currentVolume == 0) {
+            currentVolume = 0;
+        }
+    }
 }
--- src/test/java/ru/netology/homework/RadioTest.java
+++ src/test/java/ru/netology/homework/RadioTest.java
@@ -9,13 +9,17 @@ public class RadioTest {
 
     //Тесты Блока 1. Радиостанции
     // Изменение радиостанции вручную
-    @Test
-    public void shouldSetRadioStation() {
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0",
+            "1, 1",
+            "10, 0"
+    })
+    public void shouldSetRadioStation(int currentRadioStation, int expected) {
         Radio radioStation = new Radio();
 
-        radioStation.setCurrentRadioStation(8);
+        radioStation.setCurrentRadioStation(currentRadioStation);
 
-        int expected = 8;
         int actual = radioStation.currentRadioStation;
 
         Assertions.assertEquals(expected, actual);
@@ -29,57 +33,89 @@ public class RadioTest {
             "0, 1",
             "1, 2"
     })
-    public void shouldSetNextRadioStation(int currentRadioStation, int expected) {
+    public void shouldSetNextRadioStationAll(int currentRadioStation, int expected) {
         Radio radioStation = new Radio();
 
+        radioStation.setCurrentRadioStation(currentRadioStation);
         radioStation.nextRadioStation();
 
         int actual = radioStation.getCurrentRadioStation();
 
         Assertions.assertEquals(expected, actual);
-
     }
-// Переключение. Prev. Граничные значения
-//    @ParameterizedTest
-//    @CsvSource({
-//            "8, 7",
-//            "9, 8",
-//            "0, 9",
-//            "1, 0"
-//    })
-//    public void shouldSetPrevRadioStation (int currentRadioStation, int expected) {
-//        Radio prev = new Radio();
-//
-//        prev.prevRadioStation();
-//
-//        int actual = prev.currentRadioStation;
-//
-//        Assertions.assertEquals(expected, actual);
-//    }
 
-//    @Test // проверка присвоения громкости
-//    public void shouldSetVolume() {
-//        Radio volume = new Radio();
-//
-//        volume.currentVolume = 15;
-//
-//        int expected = 15;
-//        int actual = volume.getCurrentVolume();
-//
-//        Assertions.assertEquals(expected, actual);
-//    }
-//
-//    @Test //проверка присвоения меньше минимума
-//    public void shouldNotSetVolumeBelowMin() {
-//        Radio volume = new Radio();
-//
-//        volume.getCurrentVolume(-1);
-//
-//        int expected = 0; // как здесь приравнять к текущей температуре, а не к 0
-//        int actual = volume.getCurrentVolume();
-//
-//        Assertions.assertEquals(expected, actual);
-//    }
+    //Prev. Граничные значения
+    @ParameterizedTest
+    @CsvSource({
+            "8, 7",
+            "9, 8",
+            "0, 9",
+            "1, 0"
+    })
+    public void shouldSetPrevRadioStation(int currentRadioStation, int expected) {
+        Radio radioStation = new Radio();
 
+        radioStation.setCurrentRadioStation(currentRadioStation);
+        radioStation.prevRadioStation();
 
+        int actual = radioStation.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Тесты блока 2. Громкость звука
+    // Присвоение громкости
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0",
+            "10, 10",
+            "11, 0"
+    })
+    public void shouldSetVolume(int currentVolume, int expected) {
+        Radio volume = new Radio();
+
+        volume.setCurrentVolume(currentVolume);
+
+        int actual = volume.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Увеличение громкости до предела 10
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "1, 2",
+            "9, 10",
+            "10, 10"
+    })
+    public void shouldUpCurrentVolume(int currentVolume, int expected) {
+        Radio volume = new Radio();
+
+        volume.setCurrentVolume(currentVolume);
+        volume.upCurrentVolume();
+
+        int actual = volume.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Уменьшение громкости до предела 0
+    @ParameterizedTest
+    @CsvSource({
+            "1, 0",
+            "2, 1",
+            "10, 9",
+            "0, 0"
+    })
+    public void shouldDownCurrentVolume(int currentVolume, int expected) {
+        Radio volume = new Radio();
+
+        volume.setCurrentVolume(currentVolume);
+        volume.downCurrentVolume();
+
+        int actual = volume.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
